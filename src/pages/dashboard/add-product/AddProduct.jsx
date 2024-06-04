de75e0da25219ab37086eebbb6a95e5baf54b29d
okshi <johnstarhafiz@gmail.com>
2024-06-04 18:45:57 +0600

    Post product from client side to database successfully

--- src/pages/dashboard/add-product/AddProduct.jsx
+++ src/pages/dashboard/add-product/AddProduct.jsx
@@ -1,8 +1,137 @@
+import toast from "react-hot-toast";
 import "./AddProduct.css";
+
 const AddProduct = () => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    const title = form.title.value;
+    const image = form.image_url.value;
+    const price = form.price.value;
+    const delPrice = form.del_price.value;
+    const ratings = form.ratings.value;
+    const offSale = form.off_sale.value;
+    const category = form.category.value;
+    const company = form.company.value;
+
+    const data = {
+      title,
+      image,
+      price,
+      delPrice,
+      ratings,
+      offSale,
+      category,
+      company,
+    };
+    console.log(data);
+
+    await fetch("http://localhost:5000/products", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        if (data.acknowledged) {
+          toast.success("Product Added Successfully");
+        }
+
+        form.reset();
+      });
+  };
+
   return (
     <div>
-      <h1>Add Products</h1>
+      <h1 className="text-5xl font-bold text-center">Add a Product</h1>
+
+      <div className="my-16">
+        {/* form start  */}
+
+        <form onSubmit={handleSubmit} className="w-8/12 mx-auto ">
+          <div className="relative z-0 w-full mb-5 group">
+            <input
+              type="text"
+              name="title"
+              placeholder="Title"
+              id="floating_email"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            />
+          </div>
+
+          <div className="relative z-0 w-full mb-5 group">
+            <input
+              type="text"
+              name="company"
+              placeholder="Company"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            />
+          </div>
+          <div className="grid md:grid-cols-2 md:gap-6">
+            <div className="relative z-0 w-full mb-5 group">
+              <input
+                type="number"
+                name="price"
+                placeholder="Price"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              />
+            </div>
+            <div className="relative z-0 w-full mb-5 group">
+              <input
+                type="number"
+                name="del_price"
+                placeholder="Del Price"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              />
+            </div>
+          </div>
+          <div className="grid md:grid-cols-2 md:gap-6">
+            <div className="relative z-0 w-full mb-5 group">
+              <input
+                type="number"
+                name="ratings"
+                placeholder="Ratings"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              />
+            </div>
+            <div className="relative z-0 w-full mb-5 group">
+              <input
+                type="number"
+                name="off_sale"
+                placeholder="Off Sale"
+                className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              />
+            </div>
+          </div>
+
+          <div className="relative z-0 w-full mb-5 group">
+            <input
+              type="text"
+              name="category"
+              placeholder="Category"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            />
+          </div>
+          <div className="relative z-0 w-full mb-5 group">
+            <input
+              type="text"
+              name="image_url"
+              placeholder="Image URL"
+              className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            />
+          </div>
+          <button type="submit" className="btn-submit">
+            Submit
+          </button>
+        </form>
+
+        {/* form end */}
+      </div>
     </div>
   );
 };
